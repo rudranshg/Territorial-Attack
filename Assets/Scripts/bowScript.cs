5bedf8cd1095965163c85e61b2b22711ad3a0ad6
harshkp20 <harshkp20@iitk.ac.in>
2021-06-12 04:42:25 +0530

    Added Feature "Follows Mouse Pointer"
    Projectile Follows the mouse pointer

---
+++ Assets/Scripts/bowScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowScript : MonoBehaviour
+{
+    public Vector2 direction;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (MousePos - transform.position).normalized;
+        if(Input.GetAxis("Jump")!=0)  FaceMouse();
+    }
+
+    void FaceMouse()
+    {
+        transform.right = direction;
+    }
+}
