96c9a22fa6746b9cf6445f9c0d4dfc0a50a7a534
harshkp20 <harshkp20@iitk.ac.in>
2021-06-14 23:59:14 +0530

    Start Screen
    Added Start Screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  // Load next level
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit(); // Quit Game
+    }
+}
